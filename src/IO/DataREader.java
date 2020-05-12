efa8ac43bc919ca958036ed89f822973e78f1b56
lgrobelny <lukasz.grobelny@diviniti.com>
2020-05-12 14:38:52 +0200

    library

---
+++ src/IO/DataREader.java
@@ -0,0 +1,32 @@
+package IO;
+
+import Model.Book;
+
+import java.util.Scanner;
+
+public class DataREader {
+    private Scanner scan = new Scanner(System.in);
+
+    public Book readAndCreateBook(){
+        System.out.println("Tytuł");
+        String title = scan.nextLine();
+        System.out.println("Autor");
+        String author = scan.nextLine();
+        System.out.println("Wydawnicto");
+        String wydawnictwo = scan.nextLine();
+        System.out.println("Isbn");
+        String isbn = scan.nextLine();
+        System.out.println("Rok wydania");
+        int yearRelease = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Liczba stron");
+        int pages = scan.nextInt();
+        scan.nextLine();
+
+        return new Book(title, author, yearRelease, pages, wydawnictwo, isbn);
+    }
+
+    public void close(){
+        scan.close();
+    }
+}
